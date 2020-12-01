3217a478447c26a7160230dee872d503b6a9f329
Nur988 <46999128+Nur988@users.noreply.github.com>
2020-12-01 14:36:33 +0600

    Update ht.py

--- ht.py
+++ ht.py
@@ -8,8 +8,6 @@ html=""" <!DOCTYPE html>
 </style>
 <body>
 <div class="w3-content w3-display-container">
-  <img class="mySlides" src="https://www.w3schools.com/w3css/img_snowtops.jpg" style="width:100%">
-  <img class="mySlides" src="https://www.w3schools.com/w3css/img_lights.jpg" style="width:100%">
   <img class="mySlides" src="https://www.w3schools.com/w3css/img_mountains.jpg" style="width:100%">
   <img class="mySlides" src="https://www.w3schools.com/w3css/img_forest.jpg" style="width:100%">
 
@@ -46,6 +44,150 @@ header="""<!DOCTYPE html>
 
 </body>
 </html>"""
+header2="""<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+
+
+h1 {
+  color: white;
+  text-align: center;
+  text-shadow:4px 4px 6px blue;
+  font-size:65px;
+  background-color:black ;
+  border-radius:30px;
+  height:100px;
+  animation-name:example;
+  animation-duration:6s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  
+  
+}
+@keyframes example {
+  from {background-color: black;color:white;}
+  to {background-color: white;color:black}
+}
+
+p {
+  font-family: black;
+  font-size: 50px;
+  text-align:center;
+  animation-name:example;
+  animation-duration:4s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+</style>
+</head>
+<body>
+<h1>Welcome To My Website</h1>
+
+
+
+</body>
+</html>
+
+"""
+img="""<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Image</h2>
+<img src="Page_1.png" alt="Trulli" width="1000" height="800">
+
+</body>
+</html>
+"""
+header3="""<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Bootstrap Theme Simply Me</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font: 20px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+    background-color: snow; /* Green */
+    color: black;
+  }
+  .bg-2 { 
+    background-color: black; /* Dark Blue */
+    color: white;
+  }
+  .bg-3 { 
+    background-color: #ffffff; /* White */
+    color: #555555;
+  }
+  .bg-4 { 
+    background-color: #2f2f2f; /* Black Gray */
+    color: #fff;
+  }
+  .container-fluid {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+  
+  </style>
+</head>
+<body>
+
+
+
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+ <h style="font-size: 50px;">Welcome</h>
+ <h3 class="margin">Who Am I?</h3>
+ <p>I am  Chowdhury Nur -E- Alam Siddiqi</p>
+  <img src="https://assets.brand.microsites.netflix.io/assets/7f2e6102-b384-11e7-9274-06c476b5c346_cm_800w.png?v=52" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="450" height="450">
+  <h3></h3>
+</div>
+
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center">
+  <h3 class="margin">What Am I?</h3>
+  <p>I'm a Data Science Enthusiast</p>
+  
+</div>
+
+<!-- Third Container (Grid) -->
+<div class="container-fluid bg-3 text-center">    
+  <h3 class="margin">Where To Find Me?</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p>KAGGLE</p>
+      <img src="https://miro.medium.com/max/640/0*ftOal7fKVCNtJr4N.png" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>LINKEDIN</p>
+      <img src="https://img.flaticon.com/icons/png/512/174/174857.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>GITHUB</p>
+      <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--i_sb3chq--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://thepracticaldev.s3.amazonaws.com/i/fk0849hvg2rt13bpqhjy.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+  </div>
+</div>
+
+
+
+</body>
+</html>
+
+"""
 about="""<!DOCTYPE html>
 <html>
 <title>W3.CSS Template</title>
@@ -201,12 +343,8 @@ bootstrap="""<!DOCTYPE html>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <style>
-  .fakeimg {
-    height: 200px;
-    background: #aaa;
-  }
-  </style>
+
+  
 </head>
 <body>
 
@@ -225,3 +363,103 @@ bootstrap="""<!DOCTYPE html>
 
 
 """
+trial="""<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Innovation in the gameindustry">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 1</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Page-1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.0.9, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html",
+		"logo": "images/default-logo.png"
+}</script>
+    <meta property="og:title" content="Page 1">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body data-home-page="Page-1.html" data-home-page-title="Page 1" class="u-body"><header class="u-clearfix u-header u-header" id="sec-352a"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1">
+          <img src="images/default-logo.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-1.html" style="padding: 10px 20px;">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Page-2.html" style="padding: 10px 20px;">Page 2</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-1.html">Page 1</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Page-2.html">Page 2</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-section-1" src="" id="carousel_d55b">
+      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-29-lg u-size-30-xl u-size-36-md u-size-36-sm u-size-36-xs u-layout-cell-1">
+                <div class="u-container-layout u-valign-bottom-xl u-valign-top-lg u-valign-top-md u-valign-top-sm u-valign-top-xs u-container-layout-1">
+                  <h2 class="u-align-left u-text u-text-1">Innovation in the game<br>industry
+                  </h2>
+                  <p class="u-align-right u-text u-text-2">space programs<br>Tech Trends&nbsp;<br>Latest news<br>About Us<br>
+                  </p>
+                </div>
+              </div>
+              <div class="u-align-right u-container-style u-image u-layout-cell u-right-cell u-size-24-md u-size-24-sm u-size-24-xs u-size-30-xl u-size-31-lg u-image-1">
+                <div class="u-container-layout u-container-layout-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-b996"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>"""
