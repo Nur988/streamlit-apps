f33351d945e97cd35659ca3ca40cf7f8ff9f3eec
Nur988 <46999128+Nur988@users.noreply.github.com>
2020-12-02 11:39:52 +0600

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def main():
         
         st.subheader("About Me")
         image=Image.open('cv.jpg')
-        st.image(image,height=1000,width=1200)
+        st.image(image,height=1000,width=1500)
     else :
         
         #HtmlFile = open("index.html", 'r', encoding='utf-8')
@@ -55,10 +55,10 @@ def main():
         #components.html(source_code,height=800,width=800)
         #st.header("WELCOME TO MY WEBSITE")
         image = Image.open('1.jpg')
-        components.html(ht.header2,height=150,width=1200)
+        components.html(ht.header2,height=150,width=1500)
         #st.title("WELCOME TO MY WEBSITE")
-        st.image(image,width=1200)
-        components.html(ht.header3,height=1500,width=1200)
+        st.image(image,width=1500)
+        components.html(ht.header3,height=1500,width=1500)
         
                    
 
