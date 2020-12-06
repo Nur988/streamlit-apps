349314c8cd008d89a7a29557e49ae8deeb5f622d
Nur988 <46999128+Nur988@users.noreply.github.com>
2020-12-06 21:37:00 +0600

    Add files via upload

---
+++ NLP.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import os
+from textblob import TextBlob
+import spacy
+from gensim.summarization import summarize
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+def NLP():
+    def sumy_summarizer(docx):
+        parser=PlaintextParser.from_string(docx,Tokenizer("english"))
+        lex_summarizer=LexRankSummarizer()
+        summary=lex_summarizer(parser.document,3)
+        summary_list=[str(s) for s in summary]
+        result=' '.join(summary_list)
+        return result
+
+    message=st.text_area("Enter Text Here","Type Here...")  
+    options=st.selectbox("Choose Summarizer",['sumy','gensim'])
+    if st.button("Summarize"):
+        if options=='sumy':
+            st.text("using Sumy Summarizer..")
+            summary_result=sumy_summarizer(message)
+
+        elif options=='gensim':
+            st.text("Using Gensim...")
+            summary_result=summarize(message)     
+        else:
+            st.warning("Using Gensim Summarizer...")
+            summary_result=summarize(message)
+
+        st.success(summary_result)         
